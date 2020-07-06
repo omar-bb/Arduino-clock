4b6fadcd061df0115a498c03d1e446cbe1b45f17
omar-bb <omar.twobs@gmail.com>
2020-07-06 13:08:50 +0100

    Modified the header file

--- src/clock.h
+++ src/clock.h
@@ -5,6 +5,6 @@ void createArrayOfTime(unsigned char sinput[8], unsigned char* wordout);
 void Clear_Display();
 void Display(const unsigned char dat[4 * 32]);
 void Scan_Line(unsigned char m);
-void Send( unsigned char dat);
+void Send(unsigned char dat);
 
 #endif
