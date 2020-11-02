907ad98d08114e3017a0deaf7fd6dc7c81f8dcaa
Omar Ben Brahim <om3rbb@gmail.com>
2020-11-02 16:22:36 +0100

    corrected crappy style

--- include/Time.h
+++ include/Time.h
@@ -8,9 +8,16 @@
 class Time
 {
 public:
+    Time(RTC_DS3231* rtc) :
+        m_rtc(rtc) {}
+
     void InitializeRTC();
+    // parameter : char* sinput
     void GetTime(char* sinput);
     void TimeToHex(char sinput[9], unsigned char* wordout, const unsigned char refTemp[11][16]);
+
 private:
-    RTC_DS3231 _rtc;
-};#endif#endif
+    RTC_DS3231* m_rtc;
+};
+
+#endif
