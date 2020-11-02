5c7d02a4db88881741c208b2898a7644b2a2dfb1
Omar Ben Brahim <om3rbb@gmail.com>
2020-11-02 16:24:05 +0100

    using a pointer

--- src/Time.cpp
+++ src/Time.cpp
@@ -2,13 +2,14 @@
 
 void Time::InitializeRTC()
 {
-    _rtc.begin();
-    _rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    m_rtc->begin();
+    m_rtc->adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
+// parameter : char* sinput
 void Time::GetTime(char* sinput) 
 {
-	DateTime now = _rtc.now();
+	DateTime now = m_rtc->now();
 	
 	// adding a zero before the number if it has a length of 1
 	String h = (String(now.hour(), DEC).length() == 1) ? "0" + String(now.hour(), DEC) : String(now.hour(), DEC);
